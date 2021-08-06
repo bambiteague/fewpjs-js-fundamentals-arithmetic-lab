cc9e2363c8eaab62145c33af9e769487a0712b98
bambi teague <teeegsa@gmail.com>
2021-08-06 13:20:32 -0500

    commit tree 511583d251c7f9cbe68a0be7325a40731b120a9c

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+let num1 = 2
+let num2 = 31
+let num3 = 14
+let num4 = 5
+
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const mod = num3 % num4;
+
+const max = Math.max(1, 4, 6, 7, 8, 20, 15, 20);
